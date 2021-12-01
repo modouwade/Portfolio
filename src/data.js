36cf0093033e53ab42e6f8cb8c0fdd9cd62e623d
Mamadou mbaye wade <mamadou.mb.wade@gmail.com>
2021-12-01 18:36:14 +0000

    first deploy

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import page1 from "../src/img/page1.png";
+import page2 from "../src/img/page2.png";
+import page3 from "../src/img/page3.png";
+import page4 from "../src/img/page4.png";
+import page5 from "../src/img/page5.png";
+import page6 from "../src/img/page6.png";
+export const products = [
+  {
+    id: 1,
+    img: page1,
+    link: "",
+  },
+  {
+    id: 2,
+    img: page2,
+    link: "",
+  },
+  {
+    id: 3,
+    img: page3,
+    link: "",
+  },
+  {
+    id: 4,
+    img: page4,
+    link: "",
+  },
+  {
+    id: 5,
+    img: page5,
+    link: "",
+  },
+  {
+    id: 6,
+    img: page6,
+    link: "",
+  },
+];
